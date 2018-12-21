07a1ac027158ef2106bc12a6b4295fb0f71b3cc5
Mela <meliousas@gmail.com>
2018-12-21 23:24:01 +0100

    google map added

--- app/src/main/java/me/involve/involveme/HomeActivity.kt
+++ app/src/main/java/me/involve/involveme/HomeActivity.kt
@@ -5,13 +5,40 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 /**
  * Created by Mela on 2018-12-21.
  */
 
-class HomeActivity : Fragment() {
+class HomeActivity : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+
+        val sampleMarker = LatLng(-10.0, 100.0)
+        mMap.addMarker(MarkerOptions().position(sampleMarker).title("Sample marker"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sampleMarker))
+
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var mapFragment: SupportMapFragment?=null
+        mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater!!.inflate(R.layout.fragment_home, container, false)
+        var view: View = inflater?.inflate(R.layout.fragment_home, container, false)!!
+
+        return view
     }
 }
